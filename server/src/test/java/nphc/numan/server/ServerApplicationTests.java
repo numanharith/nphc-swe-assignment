34f6703424c694210a428d872656042d02e6e13d
numan <numan.h.99@gmail.com>
2022-10-23 16:15:52 +0800

    added misc files for server

---
+++ server/src/test/java/nphc/numan/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nphc.numan.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
